cea7388a43edeac2b69f1cc38a9ef6815ef68855
UWO assistant <2292597634@qq.com>
2026-09-18 12:11:54 +0800

    data: 新增梅拉蒂等五名航海士资料

--- miniprogram/generated/dataset-meta.js
+++ miniprogram/generated/dataset-meta.js
@@ -1,1 +1,1 @@
-module.exports = {"officerCount":634,"skillCount":1206,"contentVersion":"maintenance-756fe5d9f2b01a27","updatedAt":"2026-09-17T08:06:53.040Z","sourceSnapshot":"voyage-tw-2026052501"}
+module.exports = {"officerCount":639,"skillCount":1206,"contentVersion":"maintenance-756fe5d9f2b01a27","updatedAt":"2026-09-17T08:06:53.040Z","sourceSnapshot":"voyage-tw-2026052501"}
